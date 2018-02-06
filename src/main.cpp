0f917921cff5fe26e14a6bb8464f82750072eaf1
Lt-Henry <quiqueiii@gmail.com>
2018-02-06 09:31:28 +0100

    Basic hellow wolrd project

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main(int argc,char* argv[])
+{
+
+    clog<<"Twin destruction"<<endl;
+    clog<<"Copyright: Enrique Medina 2018"<<endl;
+
+    return 0;
+}
